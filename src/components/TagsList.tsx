ee81c8eff64f502c44ec346203940bb06c75b990
e.pikhterev <e.pikhterev@corp.vk.com>
2023-05-08 15:47:59 +0300

    Страница для управления фотографиями

--- src/components/TagsList.tsx
+++ src/components/TagsList.tsx
@@ -2,6 +2,7 @@ import { Group, SimpleCell } from '@vkontakte/vkui';
 import { PropsTagsList, TagItem } from './Types';
 import { DeleteTagPopout } from './DeleteTagPopout';
 import * as React from 'react';
+import { langs } from '../langs/ru';
 
 export const TagsList = ({ tags, setPopout, saveInitialState }: PropsTagsList) => {
 
@@ -12,7 +13,7 @@ export const TagsList = ({ tags, setPopout, saveInitialState }: PropsTagsList) =
           subtitle={tag.folder}
           after={<DeleteTagPopout saveInitialState={saveInitialState} tag={tag} setPopout={setPopout} />}
         >
-          {tag.title}
+          <a target="_blank" href={langs.yandex_disk_folder_link + '/' + tag.folder}></a>{tag.title}
         </SimpleCell>;
       })}
     </Group>
