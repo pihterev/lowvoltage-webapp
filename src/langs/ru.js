15ef91028d85d426b950bf92cef21258da9ad5ae
e.pikhterev <e.pikhterev@corp.vk.com>
2023-05-08 14:41:42 +0300

    Страница для управления фотографиями

--- src/langs/ru.js
+++ src/langs/ru.js
@@ -5,7 +5,7 @@ export const langs = {
   go_to_yandex_disk_action: 'Перейти на Яндекс.Диск',
   placeholder_photos_list: 'Все фотографии из чата загружены на Яндекс.Диск',
   tags_list_title: 'Теги',
-  yandex_disk_folder_link: 'https://disk.yandex.ru/client/disk/%D0%91%D1%83%D1%85%D0%B3%D0%B0%D0%BB%D1%82%D0%B5%D1%80%D0%B8%D1%8F',
+  yandex_disk_folder_link: 'https://disk.yandex.ru/client/disk/%D0%91%D1%83%D1%85%D0%B3%D0%B0%D0%BB%D1%82%D0%B5%D1%80%D0%B8%D1%8F/%D0%A1%D1%87%D0%B5%D1%82%D0%B0',
   tag_label_title: 'Название',
   tag_label_folder: 'Папка',
   save_tag_button: 'Сохранить',
